f4dfc933007460b51b10a20407b11716631fa8fa
Dan Bravender <dbravender@dbravender-desktop.(none)>
2008-08-01 11:32:51 +0900

    Refactored out a hangul library.

--- korean_verb.erl
+++ korean_verb.erl
@@ -2,102 +2,17 @@
 
 -compile(export_all).
 
-% lead = 1 + int [ (Hangul codepoint − 44032)/588 ]
-
-lead(Character) ->
-    {ok, [Base]} = utf8:from_binary(<<"ᄀ">>),
-    {ok, [X]} = utf8:from_binary(Character),
-    CodePoint = 1 + trunc((X - 44032) / 588) + Base - 1,
-    {ok, Lead} = utf8:to_binary([CodePoint]),
-    {lead, Lead, CodePoint}.
-
-% vowel = 1 + mod (Hangul codepoint − 44032 − tail, 588) / 28
-
-vowel(Character) ->
-    {ok, [Base]} = utf8:from_binary(<<"ㅏ">>),
-    {ok, [X]} = utf8:from_binary(Character),
-    {padchim, _, PadchimCodePoint} = padchim(Character),
-    {ok, [PadchimBase]} = utf8:from_binary(<<"ᆨ">>),
-    PadchimOffset = PadchimCodePoint - PadchimBase + 1,
-    CodePoint = trunc(((X - 44032 - PadchimOffset) rem 588) / 28) + Base,
-    {ok, Vowel} = utf8:to_binary([CodePoint]),
-    {vowel, Vowel, CodePoint}.
-
-% padchim = mod (Hangul codepoint − 44032, 28)
-
-padchim(Character) ->
-    {ok, [Base]} = utf8:from_binary(<<"ᆨ">>),
-    {ok, [X]} = utf8:from_binary(Character),
-    CodePoint = (X - 44032) rem 28 + Base - 1,
-    {ok, Padchim} = utf8:to_binary([CodePoint]),
-    case CodePoint of
-        4519 -> {padchim, none, 4519};
-        _ -> {padchim, Padchim, CodePoint}
-    end.
-
-join([H|Tail], Acc) ->
-    {ok, [CodePoint]} = utf8:from_binary(join(H)),
-    join(Tail, [CodePoint|Acc]);
-    
-join([], Acc) ->
-    {ok, Str} = utf8:to_binary(lists:reverse(Acc)),
-    Str.
-
-join(Characters) when is_list(Characters) ->
-    join(Characters, []);
-
-join({character, Lead, Vowel, Padchim}) ->
-    {ok, [LeadBase]} = utf8:from_binary(<<"ᄀ">>),
-    {ok, [VowelBase]} = utf8:from_binary(<<"ㅏ">>),
-    {ok, [PadchimBase]} = utf8:from_binary(<<"ᆨ">>),
-    {ok, [LeadPoint]} = utf8:from_binary(Lead),
-    {ok, [VowelPoint]} = utf8:from_binary(Vowel),
-    PadchimPoint = case Padchim of
-        none -> 
-            {ok, [X]} = utf8:from_binary(<<"ᆨ">>),
-            X - 1;
-        _ -> 
-            {ok, [X]} = utf8:from_binary(Padchim),
-            X
-    end,
-    LeadOffset = LeadPoint - LeadBase,
-    VowelOffset = VowelPoint - VowelBase,
-    PadchimOffset = PadchimPoint - PadchimBase,
-    CodePoint = PadchimOffset + (VowelOffset) * 28 + (LeadOffset) * 588 + 44032 + 1,
-    {ok, Character} = utf8:to_binary([CodePoint]),
-    Character.
-
-split([H|Tail], Acc) ->
-    {ok, Character} = utf8:to_binary([H]),
-    split(Tail, [split(Character)|Acc]);
-
-split([], Acc) ->
-    lists:reverse(Acc).
-
-split(Character) ->
-    {ok, Utf8} = utf8:from_binary(Character),
-    case length(Utf8) of
-        1 ->
-            {lead, Lead, _} = lead(Character),
-            {vowel, Vowel, _} = vowel(Character),
-            {padchim, Padchim, _} = padchim(Character),
-    
-            {character, Lead, Vowel, Padchim};
-        _ -> 
-            split(Utf8, [])
-    end.
-
 merge(Character1, Character2) when is_binary(Character1) and is_binary(Character2)->
-    SplitCharacter1 = split(Character1),
-    SplitCharacter2 = split(Character2),
+    SplitCharacter1 = hangul:split(Character1),
+    SplitCharacter2 = hangul:split(Character2),
     case is_list(SplitCharacter1) of
         true -> 
             LeadString = lists:sublist(SplitCharacter1, length(SplitCharacter1) - 1),
             EndCharacter = lists:last(SplitCharacter1),
             NewEndString = merge(EndCharacter, SplitCharacter2),
-            join(LeadString ++ NewEndString);
+            hangul:join(LeadString ++ NewEndString);
         false -> 
-            join(merge(SplitCharacter1, SplitCharacter2))
+            hangul:join(merge(SplitCharacter1, SplitCharacter2))
     end;
 
 % regulars that look irregular
@@ -165,15 +80,15 @@ merge(Character1, Character2) ->
     [Character1, Character2].
 
 past_stem(Verb) when is_binary(Verb) ->
-    SplitVerb = split(Verb),
+    SplitVerb = hangul:split(Verb),
     case is_list(SplitVerb) of
         true -> 
             LeadString = lists:sublist(SplitVerb, length(SplitVerb) - 1),
             EndCharacter = lists:last(SplitVerb),
             NewEndString = past_stem(EndCharacter),
-            join(LeadString ++ NewEndString);
+            hangul:join(LeadString ++ NewEndString);
         false -> 
-            join(past_stem(SplitVerb))
+            hangul:join(past_stem(SplitVerb))
     end;
 
 past_stem({character, _, <<"ㅗ">>, _}=Verb) ->
@@ -195,19 +110,19 @@ past_super_formal(Verb) ->
     merge(merge(merge(past_stem(Verb), <<"습">>), <<"니">>), <<"다">>).
 
 present_simple(Verb) when is_binary(Verb) ->
-    SplitVerb = split(Verb),
+    SplitVerb = hangul:split(Verb),
     case is_list(SplitVerb) of
         true -> 
             LeadString = lists:sublist(SplitVerb, length(SplitVerb) - 1),
             EndCharacter = lists:last(SplitVerb),
             NewEndString = present_simple(EndCharacter),
-            join(LeadString ++ NewEndString);
+            hangul:join(LeadString ++ NewEndString);
         false -> 
-            join(present_simple(SplitVerb))
+            hangul:join(present_simple(SplitVerb))
     end;
 
 present_simple(Verb) when is_binary(Verb) ->
-    join(present_simple(split(Verb)));
+    hangul:join(present_simple(hangul:split(Verb)));
 
 present_simple({character, _, <<"ㅗ">>, _}=Verb) ->
     merge(Verb, {character, <<"ᄋ">>, <<"ㅏ">>, none});
@@ -231,27 +146,7 @@ propositive_super_formal(Verb) ->
 imperitive_polite(Verb) ->
     merge(merge(Verb, <<"세">>), <<"요">>).
 
-main(_Args) ->
-    {padchim, <<"ᆭ">>, 4525} = padchim(<<"않">>),
-    {padchim, none, 4519} = padchim(<<"아">>),
-    
-    {vowel, <<"ㅝ">>, _} = vowel(<<"워">>),
-    {vowel, <<"ㅚ">>, _} = vowel(<<"외">>),
-    {vowel, <<"ㅣ">>, _} = vowel(<<"인">>),
-    {vowel, <<"ㅐ">>, _} = vowel(<<"앤">>),
-    {vowel, <<"ㅏ">>, _} = vowel(<<"낫">>),
-    
-    {lead, <<"ᄂ">>, _} = lead(<<"난">>),
-    {lead, <<"ᄀ">>, _} = lead(<<"간">>),
-    {lead, <<"ᄈ">>, _} = lead(<<"빨">>), 
-    
-    {character, <<"ᄂ">>, <<"ㅏ">>, <<"ᆺ">>} = split(<<"낫">>),
-    {character, <<"ᄂ">>, <<"ㅝ">>, <<"ᆯ">>} = split(<<"눨">>),
-    <<"눨">> = join({character, <<"ᄂ">>, <<"ㅝ">>, <<"ᆯ">>}),
-    <<"눠">> = join({character, <<"ᄂ">>, <<"ㅝ">>, none}),
-    <<"낫">> = join({character, <<"ᄂ">>, <<"ㅏ">>, <<"ᆺ">>}),
-    <<"몰">> = join([{character, <<"ᄆ">>, <<"ㅗ">>, <<"ᆯ">>}]),
-    
+main(_Args) ->  
     <<"워">> = merge(<<"우">>, <<"어">>),
     <<"나아">> = merge(<<"낫">>, <<"아">>),
     <<"봐">> = merge(<<"보">>, <<"아">>),
